bd42d41a295ae9610c24bcadd6ae595e45e56ed3
rssujay <40201586+rssujay@users.noreply.github.com>
2018-11-01 21:32:20 +0800

    Add files via upload

--- movement_fns.ino
+++ movement_fns.ino
@@ -1,10 +1,11 @@
 #include "MeMCore.h"
-#define OPSPD 250 // Operating Speed
-#define TDURATION 360 // Standard time taken for 90 degree turn
+#define OPSPD 210 // Operating Speed
+#define TDURATION 340 // Standard time taken for 90 degree turn
 
 //Global variables
 MeDCMotor leftWheel(M1);
-MeDCMotor rightWheel(M2); 
+MeDCMotor rightWheel(M2);
+MeLineFollower lineFinder(PORT_2);
 
 //Function prototypes for those with default arguments
 void leftTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION);
@@ -12,18 +13,33 @@ void rightTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION);
 
 void setup()
 {
+  //start off slower
+  moveForward();
 }
 
-void loop(){
-  stepForward(1000);
-  leftTurn();
-  stepForward(1000);
-  rightTurn();
-  stepForward(1000);
-  uTurn();
-  LLTurn();
-  RRTurn();
-  delay(5000);
+void loop(){ 
+  detectChallenge();
+  lineFinder.readSensors();
+  delay(1000);
+}
+
+// Challenge Solver
+void solveChallenge(void){
+  rightTurn(); 
+  stepForward(500);
+}
+
+//Detector functions
+bool lineDetect(void){
+  int sensorState = lineFinder.readSensors();
+  return (sensorState > 2);
+}
+void detectChallenge(void){ 
+  while(!lineDetect()){
+    moveForward();
+  }
+  stopWheels();
+  solveChallenge();
 }
 
 //General movement functions
@@ -54,20 +70,20 @@ void rightTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION){
   leftTurn(-turnSpeed);
 }
 
-void uTurn(){ //should this be rewritten in terms of 2x left or right turns with 0 delay?
+void uTurn(void){ //should this be rewritten in terms of 2x left or right turns with 0 delay?
   leftWheel.run(OPSPD);
   rightWheel.run(OPSPD);
   delay(TDURATION * 2);
   stopWheels();
 }
 
-void LLTurn(){
+void LLTurn(void){
   leftTurn();
   stepForward(500); //replace with dynamic value from get_duration (from frontal US emitter/detectors)
   leftTurn();
 }
 
-void RRTurn(){
+void RRTurn(void){
   rightTurn();
   stepForward(500); //replace with dynamic value from get_duration (from frontal US emitter/detectors)
   rightTurn();
