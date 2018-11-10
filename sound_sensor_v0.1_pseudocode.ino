21705ee81a45a9ab8cda842ef3384b212c96b4f3
ryanYtan <42381062+ryanYtan@users.noreply.github.com>
2018-11-10 18:18:09 +0800

    Update sound_sensor_v0.1_pseudocode.ino

--- sound_sensor_v0.1_pseudocode.ino
+++ sound_sensor_v0.1_pseudocode.ino
@@ -1,20 +1,44 @@
-// pseudocode
+#include <math.h>
+#define SND_THSHOLD 100
+
 void setup() {
-  pinMode(pin1, input)
-  pinMode(pin2, input)
+  pinMode(A0, INPUT);   // 300Hz
+  pinMode(A1, OUTPUT);  // 3000Hz
 }
 
-void loop() {
-  fa = analogRead(pin1) // (fa is 300hz)
-  fb = analogRead(pin2) // (fb is 3000hz)
+void soundSense() {
+  int fA = analogRead(A0);  // 300Hz
+  int fB = analogRead(A1);  // 3000Hz
 
-  if abs(fa - fb) > threshold // threshold is variance of loudness between fa and fb
-    // same loudness
-    left turn
-  else if abs(fb - fa) > 100
-    // fb is louder than fa
-    right turn  
-  else if abs(fa - fb) < 100
-    // fa is louder than fb
-    uturn
+  if (abs(fA - fB) < SND_THSHOLD) {
+    // {fA & fB same loudness)
+    leftTurn();
+  } else if (fB > fA) {
+    // {fB is louder}
+    rightTurn();
+  } else if (fA > fB) {
+    // {fA is louder}
+    uTurn();
+  }
+}
+
+// pseudocode
+//void setup() {
+//  pinMode(pin1, input)
+//  pinMode(pin2, input)//
+//}
+//
+//void loop() {
+//  fa = analogRead(pin1) // (fa is 300hz)
+//  fb = analogRead(pin2) // (fb is 3000hz)
+//
+//if abs(fa - fb) > threshold // threshold is variance of loudness between fa and fb
+//    // same loudness
+//    left turn
+//  else if abs(fb - fa) > 100
+//    // fb is louder than fa
+//    right turn  
+//  else if abs(fa - fb) < 100
+//    // fa is louder than fb
+//   uturn
 }
