757d67248556689054bba1709a715c46475e4ae1
ryanYtan <42381062+ryanYtan@users.noreply.github.com>
2018-11-13 11:11:41 +0800

    Update snd_sensor.ino

--- snd_sensor.ino
+++ snd_sensor.ino
@@ -1,4 +1,3 @@
-#include <math.h>
 #define SOUND_LW A0 // A0 && A1 are the pins associated with the microphone
 #define SOUND_HI A1 // if not A0 and A1, just use a different pin
 #define 
@@ -26,13 +25,10 @@ void soundSense() {
   delay(1000);
 
   int r = fL / fH;
-  if (r > 1.25)
-    // {300Hz > 3kHz)
+  if (r > 1.25)             // {300Hz > 3kHz)
     Serial.println("/-- FL IS LOUDER --/");
-  if (r < 0.75)
-    // {3kHz > 300Hz}
+  if (r < 0.75)             // {3kHz > 300Hz}
     Serial.println("/-- FH IS LOUDER --/");
-  if (r > 0.75 || r < 1.25)
-    // {3kHz same as 300Hz}
+  if (r > 0.75 || r < 1.25) // {3kHz same as 300Hz}
     Serial.println("/-- SAME LOUDNESS --/");
 }
