37fa3f9cea3fc64c3f1ecdeeb0a90aabbd76fb3f
rssujay <40201586+rssujay@users.noreply.github.com>
2018-11-13 11:30:36 +0800

    Update mastercode.ino

--- mastercode.ino
+++ mastercode.ino
@@ -1,29 +1,30 @@
 #include "MeMCore.h"
-#define OPSPD 245 // Operating Speed
-#define LOSPD 200 // Low Speed
+#define OPSPD 235 // Operating Speed
+#define LOSPD 165 // Low Speed
 #define TDURATION 290 // Standard time taken for 90 degree turn
 
 
 //Global variables
-
 MeDCMotor leftWheel(M1); // MOVEMENT
 MeDCMotor rightWheel(M2);
 MeLineFollower lineFinder(PORT_2);
 MePort IR(PORT_3);
+int8_t ULTRASONIC = 12;
+int8_t LIR, RIR; // IR
+float leftBound, rightBound;
+int a=0,b=0;
+
 
 MeRGBLed rgbled_7(7);  // COLOUR
 MeLightSensor lightsensor_6(6);
 double red, blue, green,white;
-
-int8_t LIR, RIR; // IR
-float leftBound, rightBound;
 
 //Function prototypes for those with default arguments
 void leftTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION);
 void rightTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION);
 
 void setup(){
-  leftBound = IR.aRead1()/51.0 - 1.25, rightBound = IR.aRead2()/51.0 - 1.25;
+  leftBound = IR.aRead1()/51.0 - 1.0, rightBound = IR.aRead2()/51.0 - 1.0;
   Serial.begin(9600);
   pinMode(LIR, INPUT);
   pinMode(RIR, INPUT);
@@ -36,7 +37,8 @@ void loop(){
   if(lineFinder.readSensors() != 3){
     stopWheels();
     solveChallenge();
-  }
+}
+  Serial.println(ultraSound());
   moveForward();
 }
 
@@ -50,24 +52,42 @@ void stopWheels(void){
   leftWheel.stop();
   rightWheel.stop();
 }
+
+int16_t ultraSound(void){
+  pinMode(ULTRASONIC, OUTPUT);
+  digitalWrite(ULTRASONIC, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC, LOW);
+  pinMode(ULTRASONIC, INPUT);
+  return pulseIn(ULTRASONIC, HIGH);
+}
   
 void moveForward(void){
   float left = IR.aRead1()/51.0, right = IR.aRead2()/51.0; // divide by 255*5? -- why?
-  Serial.print(left);
-  Serial.print(", ");
-  Serial.print(right);
-  Serial.print("\n");
-
-  if (right < rightBound){
+  if (right < rightBound){ //crr to left
+    b=0;
     leftWheel.run(-LOSPD);
     rightWheel.run(OPSPD);
-
+    a++;
   }
-  else if (left < leftBound){
+  else if (left < leftBound){ //crr to right
+    a=0;
+    leftWheel.run(-OPSPD);
+    rightWheel.run(LOSPD);
+    b++;
+  }
+  else if (a>100){
     leftWheel.run(-OPSPD);
     rightWheel.run(LOSPD);
   }
+  else if (b>100){
+    leftWheel.run(-LOSPD);
+    rightWheel.run(OPSPD);
+  }
   else{
+    
     leftWheel.run(-OPSPD);
     rightWheel.run(OPSPD);
   }
@@ -85,10 +105,25 @@ void leftTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION){
   rightWheel.run(turnSpeed);
   delay(duration);
   stopWheels();
+  long dist = ultraSound();
+  while (dist > 2000 && dist < 4200){
+    leftWheel.run(-OPSPD + 50);
+    rightWheel.run(-LOSPD + 50);
+    dist = ultraSound();
+  }
 }
 
 void rightTurn(int16_t turnSpeed = OPSPD, uint16_t duration = TDURATION){
-  leftTurn(-turnSpeed, duration);
+  leftWheel.run(-turnSpeed);
+  rightWheel.run(-turnSpeed);
+  delay(duration);
+  stopWheels();
+  long dist = ultraSound();
+  while (dist > 2000 && dist < 4200){
+    leftWheel.run(LOSPD - 50);
+    rightWheel.run(LOSPD - 50);
+    dist = ultraSound();
+  }
 }
 
 void uTurn(void){
@@ -100,13 +135,19 @@ void uTurn(void){
 
 void LLTurn(void){
   leftTurn();
-  stepForward(700); //replace with dynamic value from get_duration (from frontal US emitter/detectors)?
+  long time = millis();
+  while(ultraSound() > 400 || millis()-time < 800){
+    stepForward(100);
+  }
   leftTurn();
 }
 
 void RRTurn(void){
   rightTurn();
-  stepForward(700);
+  long time = millis();
+  while(ultraSound() > 400 || millis()-time < 800){
+    stepForward(100);
+  }
   rightTurn();
 }
 
@@ -114,7 +155,7 @@ void colourAction() {
   // scan
   rgbled_7.setColor(0,0,0,0);
   rgbled_7.show();
-  delay(1000);
+  delay(300);
   white = lightsensor_6.read();
   Serial.print(white);
   Serial.println(" : WHITE VALUE");
@@ -122,7 +163,7 @@ void colourAction() {
   // scan red
   rgbled_7.setColor(0,255,0,0);
   rgbled_7.show();
-  delay(1000);
+  delay(300);
   red = lightsensor_6.read();
   Serial.print(red);
   Serial.println(" : RED VALUE");
@@ -130,7 +171,7 @@ void colourAction() {
   // scan green
   rgbled_7.setColor(0,0,255,0);
   rgbled_7.show();
-  delay(1000);
+  delay(300);
   green = lightsensor_6.read();
   Serial.print(green);
   Serial.println(" : GREEN VALUE");
@@ -138,7 +179,7 @@ void colourAction() {
   // scan blue
   rgbled_7.setColor(0,0,0,255);
   rgbled_7.show();
-  delay(1000);
+  delay(300);
   blue = lightsensor_6.read();
   Serial.print(blue);
   Serial.println(" : BLUE VALUE");
@@ -147,7 +188,7 @@ void colourAction() {
   if (red > 500 && green > 500 && blue > 500) {
     Serial.println(" WHITE");
      uTurn();
-  } else if (red < 200 && green < 200 && blue < 200) {
+  } else if (red < 350 && green < 350 && blue < 350) {
     Serial.println(" BLACK");
      stopWheels();
      //play_music();
@@ -158,21 +199,21 @@ void colourAction() {
   } else if (green > red - 50 && green > blue) {
     Serial.println(" GREEN");
       rightTurn();
-  } else if (white < 180) { // one side blocked
-    if (red + 20 < 560) {
-      Serial.println(" RED");
-      leftTurn();
-    } else {
-    Serial.println(" ORANGE");
-      LLTurn();
-    }
+//  } else if (white < 180) { // one side blocked
+//    if (red + 20 < 560) {
+//      Serial.println(" RED");
+//      leftTurn();
+//    } else {
+//    Serial.println(" ORANGE");
+//      LLTurn();
+//    }
   } else { // both sides clear (white > 180)
-    if (red < 565) {
+//    if (red < 565) {
       Serial.println(" RED");
       leftTurn();
-    } else {
-      Serial.println(" ORANGE");
-      LLTurn(); 
-    }
+//    } else {
+//      Serial.println(" ORANGE");
+//      LLTurn(); 
+//    }
   }
 }
